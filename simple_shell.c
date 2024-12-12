5ffaa50e3fd015a64a2138c6c7e15394a5da236f
root <ktmqliu@gmail.com>
2024-12-12 13:21:28 +0000

    Backup

--- simple_shell.c
+++ simple_shell.c
@@ -2,13 +2,16 @@
 
 int main (void)
 {	
-	char input[120];
+	char *input; /* input from terminal */
+	char *argv[100]; /* execve arguments */
 
 	while (1) /* neverending loop for prompt */
 	{
 		printf("$ ");
 		input = getline_process();
-		strtok();	
+		strtok_process();	
+
+	}
 }
 
 /**
@@ -39,7 +42,23 @@ char *getline_process(void)
 			continue;
 		}
 	}
-	
+	if (buffer[nread - 1] == '\n') /* remove newline at the end */
+	{
+		buffer[nread - 1] == '\0';
+	}
 	return (buffer);
 }
 
+void strtok_process(char *input, char **argv)
+{
+	char *token;
+	int count = 0;
+
+	token = strtok(input, " "); /* start tokenizing with space as delimiter */
+	while (token != NULL) 
+	{	count = count + 1;
+        	argv[count] = token; /* store token into argv[count] */
+        	token = strtok(NULL, " "); /* continue tokenizing 
+    	}
+
+} 
