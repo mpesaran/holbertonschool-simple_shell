e2771418e9f79594468c086469e3f652229d29fd
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-12-19 14:38:51 +1100

    testing updated ubuntu version

--- main.c
+++ main.c
@@ -4,42 +4,29 @@
 * main - Entry point for simple shell
 * Return: Always '0'
 */
-int main(void) {
+int main(void) 
+{
 	char *command_line;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			print_prompt();
-		
-		command_line = get_user_input();
+		print_prompt();
 
-		/* EOF */
+		command_line = get_user_input();
 		if (command_line == NULL)
 		{
-			if	(isatty(STDIN_FILENO))
+			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			free(command_line); /* for valgrind */
-			break;
+			return 0;
 		}
-
-		/* skip processing IF only spaces or no input */
-		if (is_only_spaces(command_line) == 1)
-		{	
-			free(command_line); /* for valgrind */
-			break;
-		}
-
-
+		
 		remove_trailing_spaces(command_line);
-
-		/* Execute command */
-		if (strlen(command_line) > 0)
+		if (!is_only_spaces(command_line))
+		{
 			execute_command(command_line);
-
-		free(command_line);	
+		}
+		free(command_line);
 	}
-
 	return 0;
 }
  
