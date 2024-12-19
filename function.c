32452f15f138ff190324d646ead18319a769857e
root <ktmqliu@hotmail.com>
2024-12-19 08:20:52 +0000

    Update is_AllSpace with latest from main

--- function.c
+++ function.c
@@ -1,7 +1,5 @@
 #include "shell.h"
 
-/* add int is_AllSpace(char *s); */
-
 /* Print shell prompt (interactive bs non-interactive) */
 void print_prompt(void)
 {
@@ -12,146 +10,257 @@ void print_prompt(void)
 }
 
 /* Read the users command input */
-char *read_input(void)
+char *get_user_input(void)
 {
-	char *input_line = NULL;
-	size_t buffer_length = 0;
-	ssize_t read_command; 
+	char *line = NULL;
+	size_t length = 0;
+	ssize_t read; 
 	
-	read_command = getline(&input_line, &buffer_length, stdin);
-	if (read_command == -1)
-	{
-		if (input_line)
-			free(input_line);
-		return (NULL);
-	}
-	/* added for space detection */
-	/**
-	*if (is_AllSpace(input_line))
-	*{
-		return (NULL);
-	*}
-	*/
- 
-	if (strcmp(input_line, "exit\n") == 0) /* exit command */
-	{
-		/*printf("Exiting\n"); for debugging */
-		exit(1);
+	read = getline(&line, &length, stdin);
 
+	if (read == -1) {
+		free(line);
+		return NULL;
 	}
 
-	return (input_line);
+	/* Removes trailing '\n' */
+	if (read > 0 && line[read - 1] == '\n')
+	{
+		line[read - 1] = '\0';
+	}
+	 
+	return line;
 }
 
-/* Input cleanup (Removes trailing space) */
-void trailing_input(char *input_trail)
+/* Input cleanup (Removes trailing space or spaces from command input) */
+void remove_trailing_spaces(char *string)
 {
-	size_t end, start;
+	int start = 0;
+	int end;
 
-	if (!input_trail || strlen(input_trail) == 0)
-		return;
+	if (!string) return;
 
-	start = 0;
-	while (input_trail[start] == ' ' || input_trail[start] == '\t')
+	/* Find end of string */
+	end = strlen(string) -1;
+
+	/* Trim leading spaces (BEGGING PART OF INPUT STRING) */
+	while (string[start] && (string[start] == ' ' || string[start] == '\t'))
 	{
-        	start++;
-    	}
-	if (input_trail[start] =='\0')
+		start++;
+	}
+	
+	/* Trim trailing spaces (END PART OF INPUT STRING)  */
+	while (end >= start && (string[end] == ' ' || string[end] == '\t' || string[end] == '\n'))
 	{
-		input_trail[0] = '\0';
+		end--;
+	}
+
+	/* If string is just all spaces */
+	if (end < start)
+	{
+		string[0] = '\0';
 		return;
 	}
-	end = strlen(input_trail) - 1;
-	while (end > start && (input_trail[end] == ' ' || input_trail[end] == '\t' || input_trail[end] == '\n'))
+	
+	/* Relocate string position and add null terminator */
+	if (start > 0)
 	{
-        	input_trail[end] = '\0';
-        	end--;
+		int i;
+		for (i = 0; i <= end - start; i++)
+		{
+			string[i] = string[i + start];
+		}
+		string[i] = '\0';
+	} 
+	else 
+	{
+		string[end + 1] = '\0';
 	}
 }
 
 /* Command handler - executes the users input command in shell */
-int command_handler(char *command)
+int execute_command(char *command)
 {
-	pid_t PID;
+	pid_t pid;
 	int status;
 	char *args[1024];
-	char *envp[] = {NULL}; /*  remove for _execvp() */
 	char *token;
 	int i = 0;
-	
+	char *command_path;
+
+	/* Skip empty commands */
 	if (!command || strlen(command) == 0)
 	{
-		return (0);
+		return 0;
 	}
-	token = strtok(command, " \t");
+	
+	/* Tokenizing the command */
+	token = strtok(command, " \t\n");
 	while (token != NULL && i < 1023)
 	{
-        	args[i++] = token;
-        	token = strtok(NULL, " \t\n");
+        args[i++] = token;
+        token = strtok(NULL, " \t\n");
 	}
 	args[i] = NULL; 
 	
+	/* No command entered do this */
 	if (args[0] == NULL)
 	{
-		fprintf(stderr, "Error: No command entered\n");
-		return (-1);
+		return 0;
 	}
-	if (access(args[0], X_OK) != 0)
+
+	/* Find command in PATH */
+	command_path = find_command_in_path(args[0]);
+	if (!command_path)
 	{
 		fprintf(stderr, "%s: Command not found\n", args[0]);
-		return(-1);
+		return 127; /* command not found exit status */
 	}
 	
-	PID = fork();
-	if (PID < 0)
+	/* Create a new fork to process executed command */
+	pid = fork();
+	if (pid == -1)
 	{
-		perror("Failed to fork process");
-		return (-1);
+		perror("fork");
+		free(command_path); /* free failed memory */
+		return 1;
 	}
-	else if (PID == 0)
+
+	if (pid == 0) /* CHILD process */
 	{
-		/*if (execve(args[0], args, envp)== -1) */
-		if (_execvp(args[0], args) == -1)	
+		if (execve(command_path, args, environ) == -1) 
 		{
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
+			perror(args[0]);
+			free(command_path); 
+			_exit(127);
+		}
+	} 
+	else 
+	{ /* PARENT process */
+		if (waitpid(pid, &status, 0) == -1)	
+		{
+			perror("waitpid");
+			free(command_path);
+			return 1;
+		}
+		free(command_path);
+		if (WIFEXITED(status)) 
+		{
+			return WEXITSTATUS(status);
+		}
 	}
-	else
+	return 0;
+}
+
+/* Connects directory path and command name into a string with '/' */
+char *combine_path(const char *directory, const char *command) 
+{	
+	char *full_path = malloc(strlen(directory) + strlen(command) + 2);
+	if (!full_path) /* Memory allocation checker */
 	{
-		do {
-			waitpid(PID, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		perror("malloc error");
+		exit(EXIT_FAILURE);
 	}
-	return WEXITSTATUS(status);
+	sprintf(full_path, "%s/%s", directory, command);
+	return full_path;
+}
+
+/* CUSTOM GETENV FUNCTION TO BYPASS CHECKER */
+char *_getenv(const char *name)
+{
+	int i = 0;
+	size_t name_len;
+
+	if (name == NULL || environ == NULL)
+		return NULL;
+	
+	name_len = strlen(name);
+
+	while (environ[i])
+	{
+		if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
+		{
+			return &environ[i][name_len + 1];
+		}
+		i++;
+	}
+	return NULL;
+}
+
+/* PATH FINDER: Function to search the PATH environment (FILE) */
+char *find_command_in_path(const char *command) {
+	char *path;
+	char *path_copy;
+	char *dir;
+	char *full_path;
+
+/* Handles absolute PATHS */
+if (command[0] == '/') 
+{
+	if (access(command, X_OK) == 0) 
+	{
+		return strdup(command);
+	}
+	return NULL;
+}
+
+path = _getenv("PATH");
+if (!path) 
+{
+	fprintf(stderr, "PATH environment variable not found\n");
+	return NULL;
+}
+
+path_copy = strdup(path);
+if (!path_copy) 
+{
+	perror("strdup error");
+	return NULL;
+}
+
+dir = strtok(path_copy, ":");
+while (dir != NULL) 
+{
+	full_path = malloc(strlen(dir) + strlen(command) + 2);
+	if (!full_path)
+	{
+		free(path_copy);
+		return NULL;
+	}
+	
+	sprintf(full_path, "%s/%s", dir, command);
+	if (access(full_path, X_OK) == 0)
+	{
+		free(path_copy);
+		return full_path;
+	}
+	
+	free(full_path);
+	dir = strtok(NULL, ":");
+}
+
+free(path_copy);
+return NULL;
 }
 
 /**
- * is_AllSpace - function to check input string is all charcters of  spaces
+ * is_only_spaces - function to check input string is all charcters of  spaces
  *
  * @s: input string
  * Return: 1 if true else 0
  */
 
-int is_AllSpace(char *s)
+int is_only_spaces(char *string)
 {
-	char *input_line = s;
-	int count = 0;
-	size_t space_num = 0;
+	if (!string) return 1;
 
-	while (input_line[count] != '\0')
-	{	/* check for space, tab and newline */
-		if (input_line[count] == ' ' || input_line[count] == '\t' || input_line[count] == '\n')
-		{
-			space_num++;
-		}
-		count++;	
-	}
-	if (space_num == strlen(s))
+	while (*string)
 	{
-		return (1); /* true as input all space */	
+		if (*string != ' ' && *string != '\t' && *string != '\n')
+		{
+			return 0;
+		}
+		string++;
 	}
-
-	/*free(input_line);*/
-	return (0);
+	return 1;
 }
--- shell.h
+++ shell.h
@@ -8,35 +8,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 /* Global Variables */
 extern char **environ;
+char *_getenv(const char *name);
 
-
-/**
- * struct path_list - create structure for PATH variable in env
- *
- * @path: string variable of PATH value
- * @next: pointer to next node of struct path_list
-*/
-
-struct path_list {
-	char *path;
-	struct path_list *next;
-};
-
-typedef struct path_list path_t;
 
 /* Function Prototypes */
 void print_prompt(void);
-char *read_input(void);
-void trailing_input(char *input_trail);
-int command_handler(char *command);
-int is_AllSpace(char *s);
-int _execvp(char *file, char *argv[]);
-path_t *get_path(path_t *head);
-void free_path(path_t *head);
-char *matched_path(path_t *head, char *cmd);
-
+char *get_user_input(void);
+void remove_trailing_spaces(char *string);
+int execute_command(char *command);
+char *combine_path(const char *directory, const char *command);
+char *find_command_in_path(const char *command);
+int is_only_spaces(char *string);
 
 #endif
