1f4f5e6084f3d0dfd7e494e4cec717c17b1e33ef
mpesaran <mahsa.pesaran@gmail.com>
2024-12-20 17:24:54 +1100

    Edit main.c and function.c files

--- main.c
+++ main.c
@@ -8,14 +8,16 @@
 int main(void)
 {
 	char *command_line;
+	path_list paths = {NULL, NULL};
+	
+	build_path_list(&paths);
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			print_prompt();
-		}
 		
-		command_line = get_user_input();
+		command_line = read_input();
 
 		if (command_line == NULL)
 		{
@@ -24,12 +26,17 @@ int main(void)
 			free(command_line);
 			break;
 		}
-		
-		if (!is_only_spaces(command_line))
-		{
-			execute_command(command_line);
-		}
+		if (is_AllSpace(command_line) == 1)
+                {
+                        free(command_line);/* for valgrind */
+                        break;
+                }
+		trailing_input(command_line);
+
+		if (strlen(command_line) > 0)
+			command_handler(command_line, &paths);
 		free(command_line);
 	}
-	return 0;
+	free_path_list(&paths);
+	return (0);
 }
